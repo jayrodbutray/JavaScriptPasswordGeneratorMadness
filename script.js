ff6cfd1cfc58180bceb816dfe6c0aa1ec287aabf
JayRodButray <jayrodbutray@gmail.com>
2023-05-17 13:33:42 -0700

    added notes to organize requests

--- script.js
+++ script.js
@@ -10,32 +10,16 @@ var specialCharacters = "!@#$%^&*(),.?+=-"
 
 
 function generatePassword() {
-  var uppercaseLetters = confirm("Would you like UPPERcase letters? ");
-  var lowercaseLetters = confirm("Would you like lowerCASE letters? ");
-  var specialCharacters = confirm("Would you like special characters? ");
-  var numbers = confirm("Would you like numbers? ");
-  var passwordLength = prompt("Choose password length of at least 8 characters and no more than 128 characters. ");
-
-  if(numbers) {
-    generatePassword += randomNumber();
-  }
-  if(uppercaseLetters){
-    generatePassword += randomNumber();
-  }
-  if(lowercaseLetters) {
-    generatePassword += randomNumber();
-  }
-  if(specialCharacters) {
-    generatePassword =+ randomNumber();
-  }
-  for (var i=0; i<= passwordLength; i++){
-    var randomNumber = Math.floor(Math.random() * characters.length);
-    return password
-  }
-
-
-
-
+//fetching user input
+var characters = "";
+var passwordLength = 8;
+var password = "";
+  userrequestuppercaseLetters = confirm("Would you like UPPERcase letters? ");
+  userrequestlowercaseLetters = confirm("Would you like lowerCASE letters? ");
+  userrequestspecialCharacters = confirm("Would you like special characters? ");
+  userrequestnumbers = confirm("Would you like numbers? ");
+  passwordLength = prompt("Choose password length of at least 8 characters and no more than 128 characters. ");
+//validating user input
   if(passwordLength < 8 || passwordLength > 128){
     alert("Your password is not between 8 and 128 characters. Please try again.");
     var passwordLength = prompt("Choose a password length of at least 8 characters and no more than 128 characters.");
@@ -43,23 +27,38 @@ function generatePassword() {
   }else if(lowercaseLetters === false && uppercaseLetters === false && numbers === false && specialCharacters === false){
     alert("You must choose at least one password criteria.")
   }
-
-
+  //building character string for password generation
+  if(userrequestnumbers) {
+    characters += numbers;
+  }
+  
+  if(userrequestuppercaseLetters){
+    characters += uppercaseLetters;
+  }
+  if(userrequestlowercaseLetters) {
+    characters += lowercaseLetters;
+  }
+  if(userrequestspecialCharacters) {
+    characters =+ specialCharacters;
+  }
+  
+  //creating password
+  for (var i=0; i< passwordLength; i++){
+    var randomNumber = Math.floor(Math.random() * characters.length);
+    var character = characters[randomNumber]
+    console.log(characters, randomNumber, character);
+    password += character
     
-  var characters = "";
-  var passwordLength = 8;
-  var password = "";
+  }
 
+  return password
 }
-
 
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-
-  
-passwordText.value = password;
+  passwordText.value = password;
 
 }
 
